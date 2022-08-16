f409a1e198ea5f335a3d29d7f29fda6799bccc04
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-08-16 09:26:36 +0100

    Add local variables in config

--- config/app.php
+++ config/app.php
@@ -6,12 +6,17 @@ use Dovu\GuardianPhpSdk\Service\PolicyService;
 return [
 
     'app' => [
-        'base_url' => 'http://localhost:3001/api/'
+        'base_url' => 'http://localhost:3001/api/',
+
+    ],
+
+    'local' => [
+        'policy_id' => '62fa6d888e60e83b2fe05c61',
+        'hmac_secret' => '1234567890',
     ],
 
     'services' => [
         'accounts' => AccountService::class,
         'policies' => PolicyService::class,
-    ]
-
+    ],
 ];
