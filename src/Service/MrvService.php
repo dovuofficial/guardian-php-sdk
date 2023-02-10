680455742ac90ddf12f36c91566cd26381cc5faa
jonwood2 <jonwood2@googlemail.com>
2023-02-10 14:43:31 +0000

    Updated MrvService.php
    Added general supply mrv route to MRV Services.

--- src/Service/MrvService.php
+++ src/Service/MrvService.php
@@ -22,6 +22,15 @@ class MrvService extends AbstractService
         return $this->httpClient->post(uri: "policies/{$policyId}/mrv/cool-farm-tool", payload: $document, jsonRequest: true);
     }
 
+    public function submitGeneralSupplyDocumentation(string $policyId, string $document)
+    {
+        if (! is_array($document)) {
+            $document = json_decode($document, true);
+        }
+
+        return $this->httpClient->post(uri: "policies/{$policyId}/mrv/general-supply-documentation", payload: $document, jsonRequest: true);
+    }
+
     public function approveMrvDocument(string $policyId, string $did)
     {
         return $this->httpClient->put("policies/{$policyId}/approve/mrv/{$did}");
