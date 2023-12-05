54c248bd94d6da4d43a8873dc6e544053f8bce32
Matt Smithies <flyinggazelle@hotmail.co.uk>
2023-12-05 12:03:47 +0000

    Create StateService.php

---
+++ src/Service/StateService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Dovu\GuardianPhpSdk\Service;
+
+use Dovu\GuardianPhpSdk\Constants\EntityStatus;
+use Dovu\GuardianPhpSdk\Constants\StateQuery;
+
+class StateService extends AbstractService
+{
+    /**
+     * Filters, in addition to the status to query entities
+     *
+     * @var array
+     */
+    protected array $filters = [];
+
+    /**
+     *
+     * @param string $policyId
+     * @param StateQuery $query
+     * @return object
+     */
+    public function query(string $policyId, StateQuery $query)
+    {
+        $filters = http_build_query($this->filters);
+
+        return (object) $this->httpClient->get("policies/{$policyId}/state/{$query->value}?{$filters}");
+    }
+
+    /**
+     * Example usage: $sdk->state->status(EntityStatus::WAITING)->query($this->policyId, $query);
+     * @param EntityStatus $status
+     * @return $this
+     */
+    public function status(EntityStatus $status): static
+    {
+        $this->filters['status'] = $status->value;
+
+        return $this;
+    }
+
+    /**
+     *  Example usage:
+     *      $sdk->state
+     *          ->filters([ 'field0' => 'uuid' ])
+     *          ->status(EntityStatus::WAITING)
+     *          ->query($this->policyId, $query);
+     * @param array $filters
+     * @return $this
+     */
+    public function filters(array $filters): static
+    {
+        $this->filters = $filters;
+
+        return $this;
+    }
+}
