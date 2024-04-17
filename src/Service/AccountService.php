6a057e70dfa3b11e0ff701c7fb141f8d7bd2d9e6
Matt Smithies <flyinggazelle@hotmail.co.uk>
2024-04-17 10:03:46 +0100

    Account Service, using objects as response and new (refresh) token endpoints

--- src/Service/AccountService.php
+++ src/Service/AccountService.php
@@ -4,12 +4,12 @@ namespace Dovu\GuardianPhpSdk\Service;
 
 class AccountService extends AbstractService
 {
-    public function login($username, $password)
+    public function login($username, $password): object
     {
-        return $this->httpClient->post('accounts/login', [
+        return (object) $this->httpClient->post('accounts/login', [
                 'username' => $username,
                 'password' => $password,
-        ]);
+        ], true);
     }
 
     public function create($username, $password)
@@ -17,7 +17,14 @@ class AccountService extends AbstractService
         return $this->httpClient->post('accounts', [
                 'username' => $username,
                 'password' => $password,
-        ]);
+        ], true);
+    }
+
+    public function token($refresh_token): object
+    {
+        return (object) $this->httpClient->post('accounts/access-token', [
+            'refreshToken' => $refresh_token,
+        ], true);
     }
 
     public function role($policyId, $roleType)
