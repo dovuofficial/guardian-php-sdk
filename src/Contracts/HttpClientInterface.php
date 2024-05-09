b06a9482ac177308d3fa668254b4cd80eec41245
Matt Smithies <flyinggazelle@hotmail.co.uk>
2024-05-09 08:34:38 +0100

    Not happy with this type tracing for the interface

--- src/Contracts/HttpClientInterface.php
+++ src/Contracts/HttpClientInterface.php
@@ -6,9 +6,9 @@ use Exception;
 
 interface HttpClientInterface
 {
-    public function get(string $uri): bool|array|Exception;
+    public function get(string $uri): bool|array|Exception|null;
 
-    public function post(string $uri, array $payload = [], bool $jsonRequest = false): bool|array|Exception;
+    public function post(string $uri, array $payload = [], bool $jsonRequest = false): bool|array|Exception|null;
 
-    public function put(string $uri, array $payload = []): bool|array|Exception;
+    public function put(string $uri, array $payload = []): bool|array|Exception|null;
 }
