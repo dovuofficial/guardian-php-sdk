712e381e3da7a42bc8e5daf3a1d333a8012a5d08
mattsmithies <mattsmithies@users.noreply.github.com>
2024-05-30 15:30:01 +0000

    Fix styling

--- src/Service/SchemaService.php
+++ src/Service/SchemaService.php
@@ -2,15 +2,10 @@
 
 namespace Dovu\GuardianPhpSdk\Service;
 
-use Dovu\GuardianPhpSdk\Constants\EntityStatus;
-use Dovu\GuardianPhpSdk\Constants\GuardianRole;
-use Dovu\GuardianPhpSdk\Domain\CredentialDocumentBlock;
-
 class SchemaService extends AbstractService
 {
     public function get(string $policyId): array
     {
         return (array) $this->httpClient->get("schemas/$policyId?category=POLICY");
     }
-
 }
