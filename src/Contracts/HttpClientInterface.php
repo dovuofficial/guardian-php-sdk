97a1e53bd9b42bf080bb8aee4258f7e9c6a1b717
Matt Smithies <flyinggazelle@hotmail.co.uk>
2024-07-04 14:09:34 +0100

    Update HttpClientInterface.php

--- src/Contracts/HttpClientInterface.php
+++ src/Contracts/HttpClientInterface.php
@@ -2,13 +2,13 @@
 
 namespace Dovu\GuardianPhpSdk\Contracts;
 
-use Exception;
+use Dovu\GuardianPhpSdk\Domain\HttpClientResponse;
 
 interface HttpClientInterface
 {
-    public function get(string $uri): bool|array|Exception|null;
+    public function get(string $uri): HttpClientResponse;
 
-    public function post(string $uri, array $payload = [], bool $jsonRequest = false): bool|array|Exception|null;
+    public function post(string $uri, array $payload = [], bool $jsonRequest = false): HttpClientResponse;
 
-    public function put(string $uri, array $payload = []): bool|array|Exception|null;
+    public function put(string $uri, array $payload = []): HttpClientResponse;
 }
