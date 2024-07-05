d1ad2a289d14425a25bbdb03b61cba70df06af65
Matt Smithies <flyinggazelle@hotmail.co.uk>
2024-07-05 17:53:05 +0100

    Update AbstractWorkflowTask.php

--- src/Workflow/Abstract/AbstractWorkflowTask.php
+++ src/Workflow/Abstract/AbstractWorkflowTask.php
@@ -3,15 +3,51 @@
 namespace Dovu\GuardianPhpSdk\Workflow\Abstract;
 
 use Dovu\GuardianPhpSdk\Domain\CredentialDocumentBlock;
+use Dovu\GuardianPhpSdk\Support\EntityStateWaitingQuery;
 use Dovu\GuardianPhpSdk\Workflow\Contracts\GuardianWorkflowAction;
 use Dovu\GuardianPhpSdk\Workflow\Model\GuardianActionTransaction;
 use Dovu\GuardianPhpSdk\Workflow\Model\WorkflowActionResult;
 
 abstract class AbstractWorkflowTask implements GuardianWorkflowAction
 {
+    private EntityStateWaitingQuery $requiredStateQuery;
+
     protected function __construct(
         public GuardianActionTransaction $transaction
     ) {
+        $this->requiredStateQuery = EntityStateWaitingQuery::instance();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function ensureStatusRequirement(CredentialDocumentBlock $block): bool
+    {
+        $state = $this->requiredStateQuery;
+
+        if (! $state->canAttemptQuery()) {
+            throw new \Exception('Entity query attempts exhausted.');
+        }
+
+        $status_required = $this->transaction->element->statusRequirement();
+
+        if (! $status_required) {
+            return true;
+        }
+
+        $has_status = $block->hasExpectedStatus($status_required);
+
+        if ($has_status) {
+            return true;
+        }
+
+        ray("reattempt in $state->wait_seconds seconds");
+
+        sleep($state->wait_seconds);
+
+        $state->incrementAttempt();
+
+        return false;
     }
 
     public function send(array $data): object
@@ -54,7 +90,24 @@ abstract class AbstractWorkflowTask implements GuardianWorkflowAction
             $workflow->filterByTag($element_filter->tag, $filter_value);
         }
 
-        return $workflow->dataByTagToDocumentBlock($tx->element->sourceTag());
+        ray('before $block');
+
+        $block = $workflow->dataByTagToDocumentBlock($tx->element->sourceTag());
+
+        if (! $block) {
+            $state = $this->requiredStateQuery;
+            ray("'NO DATA FOUND' for block reattempt in $state->wait_seconds seconds");
+
+            sleep($state->wait_seconds);
+
+            return $this->fetchBlockDataUsingOptionalFilter();
+        }
+
+        if (! $this->ensureStatusRequirement($block)) {
+            return $this->fetchBlockDataUsingOptionalFilter();
+        }
+
+        return $block;
     }
 
     public function updateApprovalBlockState(CredentialDocumentBlock $data): CredentialDocumentBlock
