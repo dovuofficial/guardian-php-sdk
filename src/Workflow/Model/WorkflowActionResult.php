628c79cbd6422020f9ffdb6e19d3ae05543e188f
Matt Smithies <flyinggazelle@hotmail.co.uk>
2024-09-02 16:51:22 +0100

    Update WorkflowActionResult.php

--- src/Workflow/Model/WorkflowActionResult.php
+++ src/Workflow/Model/WorkflowActionResult.php
@@ -7,4 +7,14 @@ class WorkflowActionResult
     public function __construct(public $result)
     {
     }
+
+    public function getDocument(): object
+    {
+        return (object) $this->result->document;
+    }
+
+    public function getId(): string
+    {
+        return $this->getDocument()->id;
+    }
 }
